5f76cb7098b27abb60bb5df85d3b66b17c123028
MatteoNocera <matteo_nocera@tiscali.it>
2023-08-28 15:17:27 +0200

    initial commit
    create new files and connect them

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+/* 
+
+Gioco dei dadi
+
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte? Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+
+Mail
+
+Chiedi all'utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+
+Bonus
+Usiamo un input e un bottone per inserire la mail e poi mostriamo i risultati in pagina.
+Consigli del giorno:
+scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
+si ma noi cosa vogliamo fare?
+torniamo a scrivere in italiano
+proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+
+*/
+
+/* 
+Tools
+- math random?
+- for
+- array numeri
+- consolelog
+- if/else
+- 
+*/
+
+
+// Genero un numero random da 1 a 6 per il giocatore e per il computer
+
+const numbersList = [1, 2, 3, 4, 5, 6];
+console.log(numbersList);
+
+// Stabilisco il vincitore
